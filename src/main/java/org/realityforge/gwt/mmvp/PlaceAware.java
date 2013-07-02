dae301612a5efb8840c267c1277c0f939d990539
Peter Donald <peter@realityforge.org>
2013-07-02 13:45:54 +1000

    Initial source import

---
+++ src/main/java/org/realityforge/gwt/mmvp/PlaceAware.java
@@ -0,0 +1,19 @@
+package org.realityforge.gwt.mmvp;
+
+import com.google.gwt.place.shared.Place;
+
+/**
+ * Service interface implemented by activities that need access to the place.
+ */
+public interface PlaceAware
+{
+  /**
+   * Attempt to change the place of an activity.
+   * The method should return false if the activity does not support changing to the specified place.
+   *
+   * @param place the place.
+   * @param <P>   the type of the place.
+   * @return true if the place was changed, false otherwise.
+   */
+  <P extends Place> boolean atPlace( final P place );
+}
